81daba3aa0a159055d58ecae43d055ec7f327d3e
Daniel Macedo de Oliveira <danielmacedodeoliveira@MacBook-Daniel.bbrouter>
2021-05-11 19:13:02 -0300

    implementacao do get de postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,8 +14,26 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  
+  postagem: Postagem = new Postagem()
+  
+  tema: Tema = new Tema()
+  
+  listsTemas : Tema[]
+
+
+  idTema: number
+  listaPostagem: Postagem[]
+
+  user: User= new User()
+  idUser  = environment.id
+
+
   constructor(
-    private router : Router //
+    private router : Router,
+    private temaservice: TemaService,
+    private postagemservice: PostagemService,
+    private auth: AuthService
   ) { }
 
 
@@ -17,7 +41,50 @@ ngOnInit() {
      if(environment.token == ''){
       this.router.navigate(['/entrar'])
       alert("Você saiu da sua conta!")
-    }  
-  }
+    }
+    this.getAllTema() 
+    this.getTodasPostagem()
 
+  }
+getAllTema(){
+  return this.temaservice.getAllTema().subscribe((resp :Tema[])=>{
+    this.listsTemas = resp
+  })
+}
+findByIdTema(){
+  this.temaservice.getByIdTema(this.idTema).subscribe((resp:Tema)=>{
+    this.tema = resp
+  })
+}
+
+getTodasPostagem(){
+  this.postagemservice.getAllPostagem().subscribe((resp:Postagem[])=>{
+    this.listaPostagem = resp
+  })
+}
+findByIdUser(){
+  this.auth.getByIdUser(this.idUser).subscribe((resp:User)=>{
+    this.user = resp
+  })
+}
+
+
+
+publicar(){
+  this.tema.id = this.idTema
+  this.postagem.tema = this.tema
+  this.user.id = this.idUser
+  this.postagem.usuario = this.user
+
+  this.postagemservice.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+    this.postagem = resp
+
+    alert("Postagem realizada com sucesso!")
+
+    this.postagem = new Postagem()
+    this.getTodasPostagem()
+  })
+}
+
+  
 }
---
+++ src/app/service/postagem.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(
+    private htpp: HttpClient
+  ) { }
+
+  token={
+    headers: new HttpHeaders().set('Authorization',environment.token)
+  }
+
+  getAllPostagem():Observable<Postagem[]>{
+    return this.htpp.get<Postagem[]>('http://localhost:8080/postagem',this.token)
+  }
+  postPostagem(postagem:Postagem):Observable<Postagem>{
+    return this.htpp.post<Postagem>('http://localhost:8080/postagem', postagem,this.token)
+  }
+
+}
